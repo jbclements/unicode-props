15df464259be42ae4f68e88696b6d51997a22d46
John Clements <clements@racket-lang.org>
2013-05-07 15:28:24 -0700

    ready to try release

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ unicode-props
 
 This package provides definitions for unicode character class predicates.
 
+Further, it includes an auto-update mechanism that automatically extracts
+character predicates from official unicode database text files.
---
+++ unicode/info.rkt
@@ -0,0 +1,12 @@
+#lang setup/infotab
+
+(define name "unicode")
+
+(define blurb '((p "This package provides definitions for several "
+                   "unicode character properties and an auto-extract "
+                   "mechanism that can generate additional ones.")))
+
+(define scribblings '(("unicode.scrbl" () (interop))))
+(define version "2013-05-07-15:26")
+(define release-notes '((p "initial release")))
+
